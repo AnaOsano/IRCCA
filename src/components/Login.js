43f04db79aacb36d062e8534cef723891e6a34a0
anajulia1901 <57754480+anajulia1901@users.noreply.github.com>
2023-08-16 16:45:32 -0300

    login-view

---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import styles from './Login.module.css';
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+  };
+
+  return (
+    <div className={styles.LoginContainer}>
+      <h2>Iniciar sesión</h2>
+      <form className={styles.LoginForm} onSubmit={handleLogin}>
+        <div>
+          <label>Nombre de usuario:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div>
+          <label className={styles.label}>Contraseña:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit">Iniciar sesión</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
